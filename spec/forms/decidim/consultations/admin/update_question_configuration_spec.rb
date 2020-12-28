017bb684e4f2c43fe4a5b3bcc5732a46eb78b78f
Laura Jaime <laura.jv@coditramuntana.com>
2020-12-28 16:52:56 +0100

    Add some specs

---
+++ spec/forms/decidim/consultations/admin/update_question_configuration_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Consultations
+    module Admin
+      describe UpdateQuestionConfiguration do
+        let(:question) { create :question }
+        let(:min_votes) { "3" }
+        let(:max_votes) { "5" }
+        let(:blank_vote) { true }
+        let(:params) do
+          {
+            question: {
+              id: question.id,
+              min_votes: min_votes,
+              max_votes: max_votes,
+              instructions_en: "Foo instructions",
+              instructions_ca: "Foo instructions",
+              instructions_es: "Foo instructions",
+              blank_vote: blank_vote
+            }
+          }
+        end
+        let(:form) { QuestionConfigurationForm.from_params(params) }
+        let(:command) { described_class.new(question, form) }
+
+        describe "when the form is not valid" do
+          before do
+            expect(form).to receive(:invalid?).and_return(true)
+          end
+
+          it "broadcasts invalid" do
+            expect { command.call }.to broadcast(:invalid)
+          end
+
+          it "doesn't update the consultation" do
+            command.call
+            question.reload
+
+            expect(question.blank_vote).not_to eq(true)
+          end
+        end
+
+        describe "when question is not valid" do
+          before do
+            expect(question).to receive(:valid?).and_return(false)
+          end
+
+          it "broadcasts invalid" do
+            expect { command.call }.to broadcast(:invalid)
+          end
+
+          it "doesn't update the consultation" do
+            command.call
+            question.reload
+
+            expect(question.blank_vote).not_to eq(true)
+          end
+        end
+
+        describe "when the form is valid" do
+          it "broadcasts ok" do
+            expect { command.call }.to broadcast(:ok)
+          end
+
+          it "updates the question" do
+            expect { command.call }.to broadcast(:ok)
+            question.reload
+
+            expect(question.min_votes).to eq(3)
+            expect(question.max_votes).to eq(5)
+            expect(question.instructions["en"]).to eq("Foo instructions")
+            expect(question.blank_vote).to eq(true)
+          end
+        end
+      end
+    end
+  end
+end
