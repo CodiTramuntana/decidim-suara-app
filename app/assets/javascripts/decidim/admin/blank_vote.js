6cc6b309fffbacbacc94e5dcf33fdd3760bd3ed0
Laura Jaime <laura.jv@coditramuntana.com>
2020-12-29 08:57:28 +0100

    Add placeholders depends main language

--- app/assets/javascripts/decidim/admin/blank_vote.js
+++ app/assets/javascripts/decidim/admin/blank_vote.js
@@ -1,7 +1,19 @@
-const PLACEHOLDERS = [
+const PLACEHOLDERS_ES = [
   'Voto en blanco',
   'Blank vote',
   'Vot en blanc'
+];
+
+const PLACEHOLDERS_CA = [
+  'Vot en blanc',
+  'Blank vote',
+  'Voto en blanco'
+];
+
+const PLACEHOLDERS_EN = [
+  'Blank vote',
+  'Vot en blanc',
+  'Voto en blanco'
 ];
 
 function blankVote(element) {
@@ -13,10 +25,21 @@ function blankVote(element) {
 }
 
 function getInputTitles (blankVote) {
-  PLACEHOLDERS.forEach((element, index) => {
-    let title = '';
-    title = document.getElementById(`response-title-tabs-title-panel-${index}`).firstChild;
-    title.value = element;
-    title.readOnly = blankVote;
-  });
+  const languageElement = document.getElementById('admin-user-menu');
+  const language = languageElement.parentElement.getAttribute("aria-label");
+
+  if (language === 'Castellano') {
+    PLACEHOLDERS_ES.forEach((element, index) => { putTitle(element, index, blankVote); });
+  } else if (language === 'English') {
+    PLACEHOLDERS_EN.forEach((element, index) => { putTitle(element, index, blankVote); });
+  } else {
+    PLACEHOLDERS_CA.forEach((element, index) => { putTitle(element, index, blankVote); });
+  }
+}
+
+function putTitle(element, index, blankVote) {
+  let title = '';
+  title = document.getElementById(`response-title-tabs-title-panel-${index}`).firstChild;
+  title.value = element;
+  title.readOnly = blankVote;
 }
