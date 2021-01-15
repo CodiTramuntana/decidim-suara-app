d0618c9276769fad724098d28c516cf875f38a54
Laura Jaime <laura.jv@coditramuntana.com>
2021-01-15 11:25:36 +0100

    Add suggested improve

--- app/overrides/decidim/consultations/admin/responses/add_blank_vote_to_consultations_responses_form.rb
+++ app/overrides/decidim/consultations/admin/responses/add_blank_vote_to_consultations_responses_form.rb
@@ -5,7 +5,7 @@ Deface::Override.new(virtual_path: 'decidim/consultations/admin/responses/_form'
                      insert_before: "erb[loud]:contains('form.translated :text_field')",
                      text: "
                         <% if current_question.blank_vote %>
-                          <%= form.check_box :blank_vote, { onchange: 'blankVote(this)' }, { data: { 'es': I18n.with_locale(:es) { t('activemodel.attributes.response.blank_vote') },
-                            'en': I18n.with_locale(:en) { t('activemodel.attributes.response.blank_vote') }, 'ca': I18n.with_locale(:ca) { t('activemodel.attributes.response.blank_vote') } } }.to_json %>
+                          <%= form.check_box :blank_vote, { onchange: 'blankVote(this)' },
+                          { data: Hash[*current_question.organization.available_locales.map{|locale| [locale, I18n.with_locale(locale.to_sym) { t('activemodel.attributes.response.blank_vote') }] }.flatten] }.to_json %>
                         <% end %>
                      ")
