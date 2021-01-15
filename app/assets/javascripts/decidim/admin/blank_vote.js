bbb36147c309bb9d71de839f24b8a2972d830472
Laura Jaime <laura.jv@coditramuntana.com>
2021-01-15 09:05:59 +0100

    Change const by translations in data attribute

--- app/assets/javascripts/decidim/admin/blank_vote.js
+++ app/assets/javascripts/decidim/admin/blank_vote.js
@@ -1,29 +1,27 @@
-const PLACEHOLDER_ES = 'Voto en blanco';
-const PLACEHOLDER_CA = 'Vot en blanc';
-const PLACEHOLDER_EN = 'Blank vote';
-
 function blankVote(element) {
+  let translations = JSON.parse(element.getAttribute("value"));
+
   if (element.checked === true) {
-    getInputTitles(true);
+    getInputTitles(true, translations);
   } else {
-    getInputTitles(false);
+    getInputTitles(false, translations);
   }
 }
 
-function getInputTitles (blankVote) {
+function getInputTitles (blankVote, translations) {
   const languageTitles = document.getElementById('response-title-tabs').getElementsByTagName('li');
 
   for (let i = 0; i < languageTitles.length; i++) {
     let element = languageTitles[i];
     switch (element.innerText) {
       case 'Castellano':
-        putTitle(PLACEHOLDER_ES, i, blankVote);
+        putTitle(translations.data.es, i, blankVote);
         break;
       case 'Català':
-        putTitle(PLACEHOLDER_CA, i, blankVote);
+        putTitle(translations.data.ca, i, blankVote);
         break;
       case 'English':
-        putTitle(PLACEHOLDER_EN, i, blankVote);
+        putTitle(translations.data.en, i, blankVote);
         break;
     }
   }
