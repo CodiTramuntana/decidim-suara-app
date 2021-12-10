6c1cdc6114bd47a49fc399d5e94d8cc3fa3ab6b3
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-12-10 18:44:12 +0100

    [FIX] Make processes decorator compatible with Decidim 0.24

---
+++ app/queries/decidim/participatory_processes/filtered_by_permissions_and_sorted.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    # This query orders assemblies by importance, prioritizing promoted
+    # assemblies.
+    class FilteredByPermissionsAndSorted < Rectify::Query
+      include FilterParticipatorySpacesHelper
+
+      def initialize(current_user, participatory_processes_query)
+        @current_user= current_user
+        @participatory_processes_query = participatory_processes_query
+      end
+
+      attr_reader :current_user
+
+      def query
+        permissions(@participatory_processes_query).sort_by(&:weight)
+      end
+    end
+  end
+end
