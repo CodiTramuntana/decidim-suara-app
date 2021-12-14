5e3362378d3ea452a50b4d1d0a054a7c22e2435a
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-12-14 15:18:01 +0100

    [FEAT] Do not allow unpermitted access to a components

---
+++ app/decorators/controllers/decidim/components/base_controller_decorator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# This decorator change the behavior of the consultations
+# to check permissions if you are not an admin user.
+Decidim::Components::BaseController.class_eval do
+  include SuaraPermissionsSupervisor
+
+  before_action :check_suara_permissions
+
+  def check_suara_permissions
+    # Suara requires users to be logged in to access the contents of the platform
+    # so current_user is mandatory, there are no anonymous users
+    render status: :forbidden unless current_user && (current_user.admin? || suara_permissions_match?(current_user, current_participatory_space))
+  end
+
+  private
+end
