84d27fea2f2d6e4f3630f547ad2a7e8e81fb7cdb
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-10-13 14:01:26 +0200

    Remember to remove doorkeeper initializer after upgrading to Decidim v0.28

---
+++ spec/features/overrides_and_customizations_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Overrides and customizations" do
+  it "remove config/initializers/doorkeeper.rb after Decidim v0.28" do
+    # remove config/initializers/doorkeeper.rb after Decidim v0.28 as it is already initialized in that version
+    expect(Decidim.version).to be < "0.28"
+  end
+end
