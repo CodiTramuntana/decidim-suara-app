87a83f84fc34d2c826348fba6d811ac75dabf697
LauraJaimeC <75725233+LauraJaimeC@users.noreply.github.com>
2022-10-24 10:58:48 +0200

    Improvement Consultation public view (#82)
    * Improvement Consultation public view
    
    * Fix consultations index

--- app/decorators/controllers/decidim/consultations/consultations_controller_decorator.rb
+++ app/decorators/controllers/decidim/consultations/consultations_controller_decorator.rb
@@ -6,6 +6,9 @@ Decidim::Consultations::ConsultationsController.class_eval do
   include SuaraPermissionsSupervisor
 
   alias_method :original_show, :show
+
+  before_action :questions_left_to_response, only: :show
+
   def show
     original_show
     render status: :forbidden unless admin_or_with_suara_permissions?(current_user, current_participatory_space)
@@ -23,5 +26,17 @@ Decidim::Consultations::ConsultationsController.class_eval do
       @consultations = Decidim::Consultation.where(id: @consultations.map(&:id))
       @consultations = paginate(@consultations)
     end
+  end
+
+  def questions_left_to_response
+    questions = current_consultation.questions.pluck(:id)
+    response_questions = Decidim::Consultations::Vote.where(decidim_consultation_question_id: current_consultation.questions.pluck(:id),
+                                                            decidim_author_id: current_user).pluck(:decidim_consultation_question_id)
+
+    @questions_left_to_response = (questions - response_questions).count
+  end
+
+  def current_participatory_space
+    current_consultation unless action_name == "index"
   end
 end
