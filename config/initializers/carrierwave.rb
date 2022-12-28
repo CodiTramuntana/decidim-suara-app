a2197e7c2c327a16d532598a07aa6d194f6362d0
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-12-28 12:53:36 +0100

    CONF: Use ENV vars and set decidim defaults in secrets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,21 +11,21 @@ CarrierWave.configure do |config|
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
-if Rails.application.secrets.aws[:access_key_id].present?
+if Rails.application.secrets.dig(:storage, :s3, :access_key_id).present?
   require "carrierwave/storage/fog"
 
-  region= Rails.application.secrets.aws[:region]
+  region= Rails.application.secrets.dig(:storage, :s3, :region)
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = "fog/aws" # required
     config.fog_credentials = {
       provider: "AWS", # required
-      aws_access_key_id: Rails.application.secrets.aws[:access_key_id], # required
-      aws_secret_access_key: Rails.application.secrets.aws[:secret_access_key], # required
+      aws_access_key_id: Rails.application.secrets.dig(:storage, :s3, :access_key_id), # required
+      aws_secret_access_key: Rails.application.secrets.dig(:storage, :s3, :secret_access_key), # required
       region: region, # optional, defaults to 'us-east-1'
       host: "s3.#{region}.amazonaws.com" # optional, defaults to nil
     }
-    config.fog_directory = Rails.application.secrets.aws[:bucket] # required
+    config.fog_directory = Rails.application.secrets.dig(:storage, :s3, :bucket) # required
     config.fog_public = true # (optional) public readability, defaults to true
     config.fog_attributes = { "Cache-Control" => "max-age=#{365.days.to_i}" } # optional, defaults to {}
   end
